b86d97f69164936c41533590653b30ccfcbfaa4a
GGwow8888 <87293905+GGwow8888@users.noreply.github.com>
2021-09-23 11:02:09 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,6 +13,8 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
+sed -i 's/5.10/5.4/' target/linux/x86/Makefile
+
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
